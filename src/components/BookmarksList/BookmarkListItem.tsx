5d1c14ad61870bb0c0a4986b9bb744fdf40acdd1
Saurabh Jayeshbhai Das <sr850847@dal.ca>
2022-06-16 20:50:35 -0300

    style: responsive modal

--- src/components/BookmarksList/BookmarkListItem.tsx
+++ src/components/BookmarksList/BookmarkListItem.tsx
@@ -12,11 +12,15 @@ const modalStyle = {
   top: "50%",
   left: "50%",
   transform: "translate(-50%, -50%)",
-  width: 360,
+  width: {
+    sm: 250,
+    md: 250,
+    lg: 800,
+  },
   bgcolor: "background.paper",
   border: "2px solid #000",
   boxShadow: 24,
-  p: 4,
+  p: 1,
 };
 
 const BookmarkListItem: React.FC<BookmarkListWithFeeds> = ({
