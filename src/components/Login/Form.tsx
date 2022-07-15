d49fea48a3b6f61509205fa7346526537cd11473
Aman Bhandari <am727005@dal.ca>
2022-07-15 15:23:47 -0300

    Aman dev

--- src/components/Login/Form.tsx
+++ src/components/Login/Form.tsx
@@ -19,7 +19,7 @@ const Form = (props: any): any => {
   const navigate = useNavigate();
 
   function login() {
-    localStorage.setItem("userID", "hp@gmail.com");
+    localStorage.setItem("userID", "s@g.com");
     setLoggedIn(true);
     if (values.email === "admin@xyz.com") {
       navigate("/adminhome");
