9ef1ca63c4d6b0f3b4b4899d4cc60e8a7618fce9
Naga Pavan Srivathsav Chevuru <ng490417@dal.ca>
2022-07-18 13:41:02 -0300

    Tags Feature

--- src/components/Post/CreatePost.tsx
+++ src/components/Post/CreatePost.tsx
@@ -1,39 +1,21 @@
 import { Stack, TextField } from "@mui/material";
 import { Typography, Grid, Button } from "@mui/material";
 import React, { useEffect, useState } from "react";
-import { TagsInput } from "react-tag-input-component";
 import SendIcon from "@mui/icons-material/Send";
 import DeleteIcon from "@mui/icons-material/Delete";
 import { useNavigate } from "react-router-dom";
-import Box from "@mui/material/Box";
-import Modal from "@mui/material/Modal";
 import axios from 'axios';
 import MenuItem from '@mui/material/MenuItem';
 import Select, { SelectChangeEvent } from '@mui/material/Select';
-import InputLabel from '@mui/material/InputLabel';
 import UtilityUser from "../Utility/UtilityUser";
-
-/*The code has been referenced from: https://mui.com/material-ui/react-modal/*/
-const style = {
-  pt: 2,
-  px: 4,
-  pb: 3,
-  transform: "translate(-50%, -50%)",
-  position: "absolute" as "absolute",
-  top: "50%",
-  left: "50%",
-  bgcolor: "background.paper",
-  width: 500,
-  boxShadow: 24,
-  border: "3px solid #000",
-};
+import ClearOutlinedIcon from '@mui/icons-material/ClearOutlined';
+import "./Post.css";
 
 const CreatePost = () => {
   const navigate = useNavigate();
 
-  const [del, setDel] = React.useState(false);
-
-  const [tags, setTags] = React.useState<string[]>();
+  const [tags, setTags] = React.useState<string[]>([]);
+  const [taginput, setTaginput] = React.useState('');
 
   const [topic, setTopic] = React.useState<string>();
   const [errorstopic, setErrostopic] = React.useState<{ name: string }>();
@@ -53,8 +35,6 @@ const CreatePost = () => {
     });
   }, []);
 
-  const handleDeleteCloseOption = () => setDel(false);
-
   const handleChangeInType = (event: SelectChangeEvent) => {
     setType(event.target.value as string);
   };
@@ -65,36 +45,36 @@ const CreatePost = () => {
     setErrosbody({name: ""});
     setErrostype({name: ""});
 
-    let err:number = 0;
+    let errorFlag:number = 0;
 
-    let str = topic || "";
-    let result1 = typeof str === "string" ? str.trim() : "";
-    if (result1.length === 0) {
+    let enteredValue = topic || "";
+    let trimmedString = typeof enteredValue === "string" ? enteredValue.trim() : "";
+    if (trimmedString.length === 0) {
       setErrostopic({ name: "Topic cannot be empty" });
-      err = err + 1;
-    }else if (result1.length < 15){
+      errorFlag = errorFlag + 1;
+    }else if (trimmedString.length < 15){
       setErrostopic({ name: "Topic should have atleast 15 characters" });
-      err = err + 1;
+      errorFlag = errorFlag + 1;
     }
 
-    str = body || "";
-    result1 = typeof str === "string" ? str.trim() : "";
-    if (result1.length === 0) {
+    enteredValue = body || "";
+    trimmedString = typeof enteredValue === "string" ? enteredValue.trim() : "";
+    if (trimmedString.length === 0) {
       setErrosbody({ name: "Body cannot be empty" });
-      err = err + 1;
-    }else if (result1.length < 30){
+      errorFlag = errorFlag + 1;
+    }else if (trimmedString.length < 30){
       setErrosbody({ name: "Body should have atleast 30 characters" });
-      err = err + 1;
+      errorFlag = errorFlag + 1;
     }
 
-    str = type || "";
-    result1 = typeof str === "string" ? str.trim() : "";
-    if (result1.length === 0) {
+    enteredValue = type || "";
+    trimmedString = typeof enteredValue === "string" ? enteredValue.trim() : "";
+    if (trimmedString.length === 0) {
       setErrostype({ name: "Type cannot be empty" });
-      err = err + 1;
+      errorFlag = errorFlag + 1;
     }
 
-    if (err === 0) {
+    if (errorFlag === 0) {
       const postBody = {
         "userId": userId ,
         "topic": topic,
@@ -107,7 +87,7 @@ const CreatePost = () => {
 
       console.log(postBody);
 
-      axios.post(`https://csci5709-answerme-backend.herokuapp.com/posts/savePost`, postBody)
+      axios.post(`http://localhost:5000/posts/savePost`, postBody)
         .then(res => {
           console.log(res);
         })
@@ -135,117 +115,155 @@ const CreatePost = () => {
     setTopic(value);
   };
 
+  // Tags
+  const deleteTag = (index:Number) => {
+    setTags(prevState => prevState.filter((tag, i) => i !== index))
+  }
+
+  const onTagsInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const {
+      target: { value },
+    } = event;
+
+    setTaginput(value);
+  };
+  
+  const onTagsKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    const editedInput = taginput.trim();
+    const { key } = event;
+
+    if (!taginput.length && key === "Backspace" && tags.length > 0) {
+      event.preventDefault();
+      const tagsCopy = [...tags]; // shallow copy
+      const deletedTag: string = tagsCopy.pop()!;
+
+      setTags(tagsCopy);
+      setTaginput(deletedTag);
+    }
+  
+    if (editedInput.length && (key === ',' || key === 'Enter') && !tags.includes(editedInput)) {
+      event.preventDefault();
+      setTags(prevState => [...prevState, editedInput]);
+      setTaginput('');
+    }
+
+  };
+
   return (
-    <Grid container spacing={4} alignItems="center" justifyContent="center">
-      <Grid item xs={12} md={7}>
+      <Grid  item xs={12} md={7} spacing={4} alignItems="center" justifyContent="center">
         <Stack
-          className="divProfileFields"
-          direction="row"
-          spacing={5}
-          mb={3}
-          mt={5}
-          sx={{ paddingLeft: "2%", paddingRight: "2%", paddingTop: "2%" }}
+          direction="column"
+          spacing={3}
+          justifyContent="flex-start"
+          alignItems="left"
+          className="grid-container"
         >
-          <Stack className="profileFields">
-            <Stack
-              direction="column"
-              spacing={3}
-              sx={{ paddingTop: "1%" }}
-              justifyContent="flex-start"
+          <Stack 
+          direction="column"
+          spacing={3}
+          sx={{ paddingTop: "1%" }}
+          justifyContent="flex-start"
+          alignItems="left" 
+          className="grid-container-child">
+          <Stack spacing={2} >
+            <Typography variant="h5" style={{ fontWeight: 600 }}>
+              Topic
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              Be specific about the topic of your post.
+            </Typography>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              value={topic}
+              onChange={handleChangeInTopic}
+              id="topic"
+              placeholder="e.g. what are wireframes?"
+              name="topic"
+              autoFocus
+              error={Boolean(errorstopic?.name)}
+              helperText={errorstopic?.name}
+            />
+          </Stack>
+          <Stack spacing={2}>
+            <Typography variant="h5" style={{ fontWeight: 600 }}>
+              Body
+            </Typography>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="body"
+              value={body}
+              onChange={handleChangeInBody}
+              multiline
+              rows={6}
+              id="body"
+              error={Boolean(errorsbody?.name)}
+              helperText={errorsbody?.name}
+            />
+          </Stack>
+          <Stack spacing={2}>
+            <Typography variant="h5" style={{ fontWeight: 600 }}>
+              Tags
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              Add tags to explain what your post is
+            </Typography>
+            <TextField
+              value={taginput}
+              placeholder="Enter a tag"
+              onKeyDown={onTagsKeyDown}
+              onChange={onTagsInputChange}
+              />
+            <div className="tag-container">
+            {tags.map((tag, index) => (
+                  <div className="tag">
+                    <p className="tag-body">{tag}</p>
+                    <button onClick={() => deleteTag(index)}><ClearOutlinedIcon fontSize="small"/></button>
+                  </div>
+              ))}
+            </div>
+          </Stack>
+          <Stack spacing={2}>
+            <Typography variant="h5" style={{ fontWeight: 600 }}>
+              Type of Post
+            </Typography>
+              <Select
+                id="select-input-id"
+                value={type}
+                label="Type"
+                onChange={handleChangeInType}
+                error={Boolean(errorstype?.name)}
+              >
+                <MenuItem value={'Social'}>Social</MenuItem>
+                <MenuItem value={'Technical'}>Technical</MenuItem>
+              </Select>
+          </Stack>
+          <Stack direction="row" spacing={1}>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              endIcon={<SendIcon />}
+              onClick={() => postClick()}
             >
-              <Stack spacing={2}>
-                <Typography variant="h5" style={{ fontWeight: 600 }}>
-                  Topic
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                  Be specific about the topic of your post.
-                </Typography>
-                <TextField
-                  margin="normal"
-                  required
-                  fullWidth
-                  value={topic}
-                  onChange={handleChangeInTopic}
-                  id="topic"
-                  placeholder="e.g. what are wireframes?"
-                  name="topic"
-                  autoFocus
-                  error={Boolean(errorstopic?.name)}
-                  helperText={errorstopic?.name}
-                />
-              </Stack>
-              <Stack spacing={2}>
-                <Typography variant="h5" style={{ fontWeight: 600 }}>
-                  Body
-                </Typography>
-                <TextField
-                  margin="normal"
-                  required
-                  fullWidth
-                  name="body"
-                  value={body}
-                  onChange={handleChangeInBody}
-                  multiline
-                  rows={6}
-                  id="body"
-                  error={Boolean(errorsbody?.name)}
-                  helperText={errorsbody?.name}
-                />
-              </Stack>
-              <Stack spacing={2}>
-                <Typography variant="h5" style={{ fontWeight: 600 }}>
-                  Tags
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                  Add tags to explain what your post is
-                </Typography>
-                <TagsInput
-                  name="tags-input"
-                  value={tags}
-                  onChange={setTags}
-                  placeHolder="Enter tags here"
-                />
-              </Stack>
-              <Stack spacing={2}>
-                <Typography variant="h5" style={{ fontWeight: 600 }}>
-                  Type of Post
-                </Typography>
-                  <Select
-                    id="select-input-id"
-                    value={type}
-                    label="Type"
-                    onChange={handleChangeInType}
-                    error={Boolean(errorstype?.name)}
-                  >
-                    <MenuItem value={'Social'}>Social</MenuItem>
-                    <MenuItem value={'Technical'}>Technical</MenuItem>
-                  </Select>
-              </Stack>
-              <Stack direction="row" spacing={1}>
-                <Button
-                  type="submit"
-                  fullWidth
-                  variant="contained"
-                  endIcon={<SendIcon />}
-                  onClick={() => postClick()}
-                >
-                  Post
-                </Button>
-                <Button
-                  href="#discard-button"
-                  fullWidth
-                  variant="outlined"
-                  startIcon={<DeleteIcon />}
-                  onClick={discardClick}
-                >
-                  Discard
-                </Button>
-              </Stack>
-            </Stack>
+              Post
+            </Button>
+            <Button
+              href="#discard-button"
+              fullWidth
+              variant="outlined"
+              startIcon={<DeleteIcon />}
+              onClick={discardClick}
+            >
+              Discard
+            </Button>
+          </Stack>
           </Stack>
         </Stack>
       </Grid>
-    </Grid>
   );
 };
 
